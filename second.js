ae080931f914c4114f6fe1dd710848480fc2083f
yuz95a <71533761+yuz95a@users.noreply.github.com>
2025-05-06 21:39:45 +0900

    Create second.js

---
+++ second.js
@@ -0,0 +1,17 @@
+[
+  {
+    "name": "Alice",
+    "age": 30,
+    "email": "alice@example.com"
+  },
+  {
+    "name": "Bob",
+    "age": 25,
+    "email": "bob@example.com"
+  },
+  {
+    "name": "Charlie",
+    "age": 28,
+    "email": "charlie@example.com"
+  }
+]
